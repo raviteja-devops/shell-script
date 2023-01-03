b95e0576ad3b98b108c964171aa4376ef327fa53
raviteja <ravitejatfcdemo@gmail.com>
2023-01-03 14:01:38 +0530

    renaming file

--- color_code.sh
+++ color_code.sh
@@ -1,3 +1,11 @@
+# 6 colors supported in shell scripting
+#red - color code-31
+#green - 32
+#yellow - 33
+#blue - 34
+#magenta - 35
+#cyan - 36
+
 echo -e "\e[31mMESSAGE\e[0m"
 echo -e "\e[32mMESSAGE\e[0m"
 echo -e "\e[33mMESSAGE\e[0m"
